a3495b5be8c31d047d639ff64762f661564dc9bc
sdrtba <sdrtba>
2025-11-01 14:26:15 +0400

    server side communication

--- shoto/vite.config.js
+++ shoto/vite.config.js
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/con fig/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-      port: 3000,
-      proxy: {
-        "/api": {
-          target: "http://localhost:8000",
-          changeOrigin: true,
-        },
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
       },
     },
+  },
 })
